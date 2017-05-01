474dcfaa6882cb6537032a43f7a9df047d123f96
Sean Enck <enckse@gmail.com>
2017-05-01 12:47:29 -0400

    redir all

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 USE_CORE=/opt/system-monitor/core
 export USE_CORE
 source $USE_CORE
-if type git > /dev/null; then
+if type git &> /dev/null; then
     cd ${LOCATION} && git pull
 fi
 ${LOCATION}mon.sh
