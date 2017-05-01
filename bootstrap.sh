097fb0688ac147772b8d709255c22b776ddb3cc2
Sean Enck <enckse@gmail.com>
2017-05-01 14:15:16 -0400

    set back to 12, force trigger enabled

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,11 @@ USE_CORE=/opt/system-monitor/core
 export USE_CORE
 source $USE_CORE
 hour=$(date +%H)
+if [ ! -z $1 ]; then
+    if [ $1 == "force" ]; then
+        hour=$TRIGGER_HOUR
+    fi
+fi
 if [ $hour -eq $TRIGGER_HOUR ]; then
     if type git &> /dev/null; then
         cd ${LOCATION} && git pull
