859904af7f5d6822f38b55f8b8fe37fabdc67fec
Sean Enck <enckse@gmail.com>
2017-05-01 14:13:42 -0400

    avoid any work on not trigger hour

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,10 @@
 USE_CORE=/opt/system-monitor/core
 export USE_CORE
 source $USE_CORE
-if type git &> /dev/null; then
-    cd ${LOCATION} && git pull
-fi
 hour=$(date +%H)
 if [ $hour -eq $TRIGGER_HOUR ]; then
+    if type git &> /dev/null; then
+        cd ${LOCATION} && git pull
+    fi
     ${LOCATION}mon.sh
 fi
