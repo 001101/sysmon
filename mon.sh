9759a4ee3403f67aeadbd274fef5cdba55d85c00
Sean Enck <enckse@gmail.com>
2017-05-01 13:47:17 -0400

    weekly reporting on successful check

--- mon.sh
+++ mon.sh
@@ -33,12 +33,22 @@ for conf in $(find $CONFD -type f -name "*.sh"); do
 done
 
 do_report="$HOSTNAME checked"
+reporting=0
 if [ -s $report ]; then
     do_report=""
     for r in $(cat $report | cut -d " " -f 1 | uniq | sort); do
         do_report=$do_report" "$r
     done
     do_report="monitor alerted: "$(echo $HOSTNAME)" -> "$(echo $do_report | sed "s/ /,/g")
+    reporting=1
+else
+    echo "$report is empty"
+    check=$(date +%u)
+    if [ $check == $WEEKLY ]; then
+        reporting=1
+    fi
 fi
-as_json=$(echo "{\"msgtype\":\"m.text\", \"body\":\""$do_report"\"}")
-curl -XPOST -d "$as_json" "$MATRIX_API/_matrix/client/r0/rooms/$MATRIX_ROOM/send/m.room.message?access_token=$MATRIX_TOKEN"
+if [ $reporting -eq 1 ]; then
+    as_json=$(echo "{\"msgtype\":\"m.text\", \"body\":\""$do_report"\"}")
+    curl -XPOST -d "$as_json" "$MATRIX_API/_matrix/client/r0/rooms/$MATRIX_ROOM/send/m.room.message?access_token=$MATRIX_TOKEN"
+fi
