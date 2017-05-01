8307450d8eaebb8c3974ed8ca8c002e544755e12
Sean Enck <enckse@gmail.com>
2017-05-01 13:21:41 -0400

    ignore trace back of the journal

--- conf.d/journal.sh
+++ conf.d/journal.sh
@@ -6,4 +6,4 @@ if [ ! -z "$journal_ignore" ]; then
     skip=$journal_ignore
     flag=-v
 fi
-journalctl -p err --since "$yesterday 12:00:00" --until "$today 12:00:00" | tail -n +2 | grep -E $flag "${skip}"
+journalctl -p err --since "$yesterday 12:00:00" --until "$today 12:00:00" | tail -n +2 |grep -v "^[ ]" | grep -E $flag "${skip}"
