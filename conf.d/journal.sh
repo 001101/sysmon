0154cce19c6d86bb90608ef2125064ec88b8851a
Sean Enck <enckse@gmail.com>
2017-06-21 12:59:45 -0400

    use yesterday at noon or last reboot

--- conf.d/journal.sh
+++ conf.d/journal.sh
@@ -6,4 +6,11 @@ if [ ! -z "$journal_ignore" ]; then
     skip=$journal_ignore
     flag=-v
 fi
-journalctl -p err --since "$yesterday 12:00:00" --until "$today 12:00:00" | tail -n +2 | grep -v "\-\- Reboot \-\-" | grep -v "^[ ]" | grep -E $flag "${skip}"
+last_reboot=$(uptime --since)
+use_since="${yesterday} 12:00:00"
+comp_last=$(date -d $(echo $last_reboot | sed "s/ /T/g") +%s)
+comp_yest=$(date -d $(echo $use_since | sed "s/ /T/g") +%s)
+if [ $comp_last -gt $comp_yest ]; then
+    use_since="$last_reboot"
+fi
+journalctl -p err --since "$use_since" --until "$today 12:00:00" | tail -n +2 | grep -v "\-\- Reboot \-\-" | grep -v "^[ ]" | grep -E $flag "${skip}"
